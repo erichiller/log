67489ca1d83c0cb82940300a24fe77f1a7f6da20
Eric D. Hiller <eric@hiller.pro>
2018-12-22 13:41:10 +0200

    checkpoint for docs. still fighting mutators

--- README.md
+++
@@ -1,7 +0,0 @@
-
-
-In the future it would also be a nice addition to have a StructuredFormatter that output in JSON or some other structured output
-
-
-# def json formatter ???
-# https://docs.python.org/3.6/howto/logging-cookbook.html#implementing-structured-logging
--- __init__.py
+++ __init__.py
@@ -12,6 +12,16 @@ https://docs.python.org/3/howto/logging-cookbook.html
 
 Existing Handlers:
 https://docs.python.org/3/howto/logging.html#useful-handlers
+
+
+
+In the future it would also be a nice addition to have a StructuredFormatter that output in JSON or some other structured output
+
+
+def json formatter ???
+
+https://docs.python.org/3.6/howto/logging-cookbook.html#implementing-structured-logging
+
 """
 
 
